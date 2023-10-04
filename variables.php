034dacbc33b7854ea5840dd984b8650e2398bc96
samy <98467290+Samy-726@users.noreply.github.com>
2023-10-04 10:29:53 +0200

    connectez PHP à MYSQL avec PDO

--- variables.php
+++ variables.php
@@ -6,22 +6,19 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'S3cr3t'
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'devine'
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
-    ],
-    [
-        'full_name' => 'samy rve',
-        'email' => 'latinosamy726@gmail.com',
-        'age' => 28,
-        'password' => 'Samy',
+        'password' => 'P4ssW0rD'
     ],
 ];
 
